291cda45eb3a84b168def03360309c43e57e61b4
Soni-Deepak <deepaksoni00450420@gmail.com>
2026-07-20 17:50:41 +0530

    completion of chapter 4 practice set

---
+++ Chapter 4 - Ps/problem2.py
@@ -0,0 +1,20 @@
+Marks = []
+
+f1 = int(input("Enter 1st Marks here :"))
+Marks.append(f1)
+f2 = int(input("Enter 2nd Marks here :"))
+Marks.append(f2)
+f3 = int(input("Enter 3rd Marks here :"))
+Marks.append(f3)
+f4 = int(input("Enter 4th Marks here :"))
+Marks.append(f4)
+f5 = int(input("Enter 5th Marks here :"))
+Marks.append(f5)
+f6 = int(input("Enter 6th Marks here :"))
+Marks.append(f6)
+f7 = int(input("Enter 7th Marks here :"))
+Marks.append(f7)
+
+Marks.sort()
+
+print(Marks)
---
+++ Chapter 4 - Ps/problem3.py
@@ -0,0 +1,3 @@
+l = [223 ,43 ,65, 324]
+
+print(sum(l)) #sum all the numbers
