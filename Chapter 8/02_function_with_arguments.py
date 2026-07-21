4889e6cc60db74ff2416089c48c7d9f00882bb3c
Soni-Deepak <deepaksoni00450420@gmail.com>
2026-07-21 20:13:26 +0530

    Function & Recursion

---
+++ Chapter 8/02_function_with_arguments.py
@@ -0,0 +1,9 @@
+def goodDay(name, ending):
+    print("Good Day " + name)
+    print(ending)
+    return "OK"
+
+a = goodDay("Harry" , "Thanku")
+print(a) #If nothing in return it will show NONE
+# goodDay("Rohan" , "Thanku")
+# goodDay("Divya" , "Thanku")
---
+++ Chapter 8/03_default_arguments.py
@@ -0,0 +1,6 @@
+def goodDay(name, ending= "Thanku"):
+    print(f"Good Day , {name}")
+    print(ending)
+   
+goodDay("Deepak soni", "Thanks") # use thanks
+goodDay("Soni") # use by default value
